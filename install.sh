81c9c916a743f294f01063d783facb443ff6c899
Tommy Pyatt <tp@amp.co>
2015-12-24 16:29:07 +0000

    Add atom dotfiles to install script

--- install.sh
+++ install.sh
@@ -7,3 +7,10 @@ ln -s "$mydir"/.gitconfig
 rm .zshrc
 ln -s "$mydir"/.zshrc
 
+mkdir -p .atom
+cd .atom
+rm config.cson
+ln -s "$mydir"/.atom/config.cson
+rm snippets.cson
+ln -s "$mydir"/.atom/snippets.cson
+
