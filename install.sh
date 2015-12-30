b1705a04ea5cdf4fc7538d3e1560403a52eaa9b0
Tommy Pyatt <tp@amp.co>
2015-12-30 17:40:31 +0000

    Re-path magerun references and create bin folder in home
    Also moved magerun download to after symlinks are created, for
    consistency.

--- install.sh
+++ install.sh
@@ -6,9 +6,10 @@ then
 
 # Confirmed, let's do it
 
-# Store my directory to for symlinking
+# Store my directory for symlinking
 mydir="$PWD"
 cd
+mkdir -p ~/bin
 
 # Create in home directory
 echo "Linking gitconfig"
@@ -23,14 +24,6 @@ echo "Linking vimrc"
 rm -f .vimrc
 ln -s "$mydir"/.vimrc
 
-if [ ! -e "bin/n98-magerun.phar" ]
-then
-    echo "Getting magerun"
-    wget http://files.magerun.net/n98-magerun-latest.phar -O bin/n98-magerun.phar
-    chmod +x bin/n98-magerun.phar
-fi
-
-
 # Create .atom directory if it doesn't exist, and put config files in there
 mkdir -p .atom
 cd .atom
@@ -42,16 +35,29 @@ ln -s "$mydir"/.atom/config.cson
 rm -f snippets.cson
 ln -s "$mydir"/.atom/snippets.cson
 
-
 # Install vim pathogen
 echo "Installing pathogen and vim-gitgutter"
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+# Install gitgutter if it isn't already there
 cd ~/.vim/bundle
 if [ ! -e "vim-gitgutter" ]
 then
     git clone git://github.com/airblade/vim-gitgutter.git
 fi
 
+# Install oh my zsh
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# Install magerun
+if [ ! -e "~/bin/n98-magerun.phar" ]
+then
+    echo "Getting magerun"
+    wget http://files.magerun.net/n98-magerun-latest.phar -O ~/bin/n98-magerun.phar
+    chmod +x ~/bin/n98-magerun.phar
 fi
+
+# End confirmation condition
+fi
+
