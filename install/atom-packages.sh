a728d638d57474255240e1da35540efc92be62c2
Tommy Pyatt <tommypyatt@users.noreply.github.com>
2017-06-29 10:17:29 +0100

    General update

--- install/atom-packages.sh
+++ install/atom-packages.sh
@@ -2,7 +2,6 @@ apm install ex-mode
 apm install minimap
 apm install multi-wrap-guide
 apm install pigments
-apm install tab-switcher
-apm install vim-mode
+apm install vim-mode-plus
 apm install atom-runner
 
